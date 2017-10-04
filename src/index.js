42b95ebe27fe79e2a38e60a3b2ef3ae18634deec
QTSEL\nnd <nate.anderson@qlik.com>
2017-10-03 22:43:18 -0600

    Getting pretty bright colors

--- src/index.js
+++ src/index.js
@@ -4,5 +4,5 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App color1="red" color2="blue" color3="yellow" color4="orange" />, document.getElementById('root'));
 registerServiceWorker();
